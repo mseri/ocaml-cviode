dbf1d7bf4cfd15dcbe3b5ee4dbc6af4b47c87541
Marcello Seri <marcello.seri@gmail.com>
2019-02-03 13:21:52 +0100

    cviode: further simplfy interface
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- src/cviode.ml
+++ src/cviode.ml
@@ -3,10 +3,6 @@
 type timespan = float * float
 (** Representation of a time span. *)
 
-(** Private function, allow to get the slices indexes for xs and ys *)
-let slices idx elts =
-  [[idx]; [0; elts/2-1]], [[idx]; [elts/2; elts-1]]
-
 let check _a _f y0 =
   (* TODO: check shapes of a and f, make sure they are all compatible *)
   let _, elts = Owl.Mat.shape y0 in
@@ -15,11 +11,14 @@ let check _a _f y0 =
 let steps t0 t1 dt =
   (t1 -. t0)/.dt |> Float.floor |> int_of_float
 
-(* opening Owl.Mat or Owl.Arr messes up badly all the integer operations *)
-let (.${}) = Owl.Mat.(.${})
-let (.${}<-) = Owl.Mat.(.${}<-)
-
 let integrate ~step y0 (t0, t1) dt =
+  (* opening Owl.Mat or Owl.Arr messes up badly all the integer operations *)
+  let (.${}) = Owl.Mat.(.${}) in
+  let (.${}<-) = Owl.Mat.(.${}<-) in
+  (* Allow to get the slices indexes for xs and ys *)
+  let slices idx elts =
+    [[idx]; [0; elts/2-1]], [[idx]; [elts/2; elts-1]]
+  in
   let steps = steps t0 t1 dt in
   let _, elts = Owl.Mat.shape y0 in
   let sol = Owl.Mat.empty steps elts in
