83b04d1ef2d7c6a8ddc99874debfd6feeb5b17a0
Marcello Seri <marcello.seri@gmail.com>
2019-02-05 19:18:24 +0100

    common: use linspace here, it is pointless to fill in the vector
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- src/common.ml
+++ src/common.ml
@@ -14,7 +14,6 @@ let integrate ~step y0 (t0, t1) dt =
   let (.${}) = Owl.Mat.(.${}) in
   let (.${}<-) = Owl.Mat.(.${}<-) in
   let (.%{}) = Owl.Arr.(.%{}) in
-  let (.%{}<-) = Owl.Arr.(.%{}<-) in
   (* Allow to get the slices indexes for xs and ys *)
   let slices idx elts =
     [[idx]; [0; elts/2-1]], [[idx]; [elts/2; elts-1]]
@@ -22,19 +21,15 @@ let integrate ~step y0 (t0, t1) dt =
   let steps = steps t0 t1 dt in
   let _, elts = Owl.Mat.shape y0 in
   let sol = Owl.Mat.empty steps elts in
-  (* These are all fixed step algorithms, saving the time array is
-     quite overkill as tspan will just be a deterministic linspace... *)
-  let tspan = Owl.Arr.empty [|steps|] in
+  let tspan = Owl.Arr.linspace t0 t1 steps in
   sol.${[[0]]}<- y0;
-  tspan.%{[|0|]}<-t0;
   for idx = 1 to steps-1 do
     let xi, pi = slices (idx-1) elts in
     let xi', pi' = slices idx elts in
     let xs, ps = sol.${xi}, sol.${pi} in
     let t = tspan.%{[|idx-1|]} in
-    let xs', ps', t' = step xs ps t in
+    let xs', ps', _ = step xs ps t in
     sol.${xi'}<- xs';
     sol.${pi'}<- ps';
-    tspan.%{[|idx|]}<-t';
   done;
   tspan, sol
